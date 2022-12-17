3822e4ce860d90cbb65e8975b2972be4bc3f8e2d
mina <teenage.web.academy@gmail.com>
2022-12-17 19:45:12 +0330

    add files

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/**
+* @type {import('next').NextConfig}
+*/
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+}
+
+module.exports = nextConfig
+
+
+
+
+///** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
